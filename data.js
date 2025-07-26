8b9b970d933ca1b21ce3c69b8b07cdf37b7b2ebb
Niy4s <Pesmader@gmail.com>
2025-07-26 16:02:31 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,118 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-liv-1",
+      "name": "LIV (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6525305827765209,
+          "pitch": 0.08658114471202083,
+          "rotation": 0,
+          "target": "1-passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-passage",
+      "name": "passage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5613784135400675,
+          "pitch": 0.1151556360121706,
+          "rotation": 0,
+          "target": "2-bedroom-1-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bedroom-1-1",
+      "name": "bedroom .1 (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
